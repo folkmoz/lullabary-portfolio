d1d58923a9aff2aac72c2cc2df7254555d71af4e
folkmoz1 <folkmoz1@gmail.com>
2024-10-09 14:09:48 +0700

    ✨ (RisingDream.tsx): update media assets and video link for Rising Dream section ♻️ (RisingDream.tsx): refactor component structure for improved layout ✨ (RisingDream.tsx): add image preloading for better performance

--- src/components/Sections/experiences/RisingDream.tsx
+++ src/components/Sections/experiences/RisingDream.tsx
@@ -4,17 +4,22 @@ import HeadlineRow from "~components/Sections/experiences/components/HeadlineRow
 import bgBlue from "~assets/experience/bg-blue.png";
 import Animation from "~components/AnimatedComponents/Animation";
 import VideoPlayer from "~components/Sections/experiences/components/Videoplayer";
-import clip from "~assets/experience/millmills/clip/millmills-clip.mp4";
 import FullLinkButton from "~components/Sections/experiences/components/FullLinkButton";
 import CustomSwiper from "~components/Sections/experiences/components/CustomSwiper";
-import bh01 from "~assets/experience/millmills/pics/bh01.jpeg";
-import bh02 from "~assets/experience/millmills/pics/bh02.jpeg";
-import bh03 from "~assets/experience/millmills/pics/bh03.jpeg";
-import bh04 from "~assets/experience/millmills/pics/bh04.jpeg";
-import bh05 from "~assets/experience/millmills/pics/bh05.jpeg";
-import bh06 from "~assets/experience/millmills/pics/bh06.jpeg";
+
+import clip from "~assets/experience/rising-dream/clip/clip.mp4";
+
+import bh01 from "~assets/experience/rising-dream/pics/bh01.webp";
+import bh02 from "~assets/experience/rising-dream/pics/bh02.webp";
+import bh03 from "~assets/experience/rising-dream/pics/bh03.webp";
+import bh04 from "~assets/experience/rising-dream/pics/bh04.webp";
+import bh05 from "~assets/experience/rising-dream/pics/bh05.webp";
+import bh06 from "~assets/experience/rising-dream/pics/bh06.webp";
+import useImagePreloader from "~hooks/useImagePreloader";
 
 function RisingDream() {
+  useImagePreloader([bh01, bh02, bh03, bh04, bh05, bh06, bgBlue]);
+
   return (
     <div className="relative z-[1] mt-14 w-full bg-secondary py-10">
       <div className="h relative mx-auto flex w-full max-w-[1200px] flex-col p-4">
@@ -31,25 +36,27 @@ function RisingDream() {
         }}
         className="shadow-custom-y mt-20 min-h-[80dvh] w-full py-10"
       >
-        <div className="w-full">
-          <div className="flex flex-col gap-2 lg:flex-row">
-            <div className="relative w-full lg:w-1/2">
-              <Animation.ImageReveal end="top 80%">
-                <VideoPlayer source={clip} />
-              </Animation.ImageReveal>
-            </div>
+        <div className="h relative mx-auto flex w-full max-w-[1200px] flex-col p-4">
+          <div className="w-full">
+            <div className="flex flex-col gap-2 lg:flex-row">
+              <div className="relative w-full lg:w-1/2">
+                <Animation.ImageReveal end="top 80%">
+                  <VideoPlayer source={clip} />
+                </Animation.ImageReveal>
+              </div>
 
-            <div className="flex w-full lg:w-1/2">
-              <div className="lg:ml-24">
-                <FullLinkButton
-                  href={"https://www.youtube.com/watch?v=AwM9Lxup52o"}
-                />
+              <div className="flex w-full lg:w-1/2">
+                <div className="lg:ml-24 lg:mt-14">
+                  <FullLinkButton
+                    href={"https://www.youtube.com/watch?v=22trnV3XqqI"}
+                  />
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="mt-2">
-            <CustomSwiper images={[bh01, bh02, bh03, bh04, bh05, bh06]} />
+            <div className="mt-2">
+              <CustomSwiper images={[bh01, bh02, bh03, bh04, bh05, bh06]} />
+            </div>
           </div>
         </div>
       </div>
