c80be3628b084b9f7eb98a3faaf6e9e197abb75f
folkmoz1 <folkmoz1@gmail.com>
2024-10-11 12:42:16 +0700

    remove vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import react from "@vitejs/plugin-react-swc";
 import preload from "vite-plugin-preload";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), ViteImageOptimizer(), preload()],
+  plugins: [tsconfigPaths(), react(), preload()],
 });
