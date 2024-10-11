be34e326c5fc2fcc990b12e9c011a85b73a95032
folkmoz1 <folkmoz1@gmail.com>
2024-10-11 14:06:40 +0700

    added plugin to optimize images

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import preload from "vite-plugin-preload";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), preload()],
+  plugins: [tsconfigPaths(), react(), ViteImageOptimizer(), preload()],
 });
